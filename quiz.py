6cc650db1f2cdb481aeac36bd301752ac84a87d6
shioncm <shioncm2001@gmail.com>
2023-07-16 04:22:25 -0400

    Finish set-up of script, needs questions - All basic functionality for the script is completed - The questions for the OCEAN/Big 5 quiz is not yet put in

--- quiz.py
+++ quiz.py
@@ -9,11 +9,54 @@ CHOICES = "INACCURATE\t1\t2\t3\t4\t5\tACCURATE"
 
 ALL_USER_CHOICES = []
 
-for num, question in enumerate(QUESTIONS, start=1):
-    print(f"\nQuestion {num}:")
-    print(f"{question}?")
-    print(CHOICES)
-    user_choice = int(input("Choice? "))
-    ALL_USER_CHOICES.add(user_choice)
+def start_screen():
+    print("To start the OCEAN quiz, enter 's'")
+    print("To exit this app, enter 'q'")
+    
+    while(True):
+        user_choice = input("Enter 's' or 'q': ")
+
+        if(user_choice == 's'):
+            run_quiz()
+        elif(user_choice == 'q'):
+            exit(0)
+        else:
+            print("Invalid input. Please try again.")
+
+def run_quiz():
+    for num, question in enumerate(QUESTIONS, start=1):
+        print(f"\nQuestion {num}:")
+        print(f"{question}?")
+        print(CHOICES)
+        while (True):
+            user_choice = input("Choice? ")
+
+            if user_choice.isnumeric():
+                ALL_USER_CHOICES.append(user_choice)
+                break
+            else:
+                print("Invalid number. Please try again.")
+    
+    end_screen()
+
+def end_screen():
+    print("\nThanks for playing!")
+    
+    while(True):
+        user_choice = input("Review your choices? (y/n) ")
+        if(user_choice == 'y'):
+            print("\n---- YOUR ANSWERS ----")
+            for num, question in enumerate(QUESTIONS, start=1):
+                print(f"\nQuestion {num}:")
+                print(f"{question}?")
+                print(ALL_USER_CHOICES[num-1])
+            
+            exit(0)
+        elif(user_choice == 'n'):
+            exit(0)
+        else:
+            print("Invalid input. Please try again.")
+
+start_screen()
 
     
