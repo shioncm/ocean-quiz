c2f52570c02325945b5753d4c3227696e19f4a37
shioncm <shioncm2001@gmail.com>
2023-07-28 07:19:15 -0400

    Fix calc_score function, input range for quiz - Fix score calculation in calc_score function, saves score to   user_scores list - Pass in user_scores list to end_screen function, print scores   (Can be improved, add more screens and options, display highest score - Fixed QUESTIONS list to have 50 questions (was previously missing a   question) - Add check to user input choice, makes sure user input an int between   1 and 5 (have not tested yet) - Added user_choices randomly generated list for debugging speed,   need to remove later

--- quiz.py
+++ quiz.py
@@ -1,3 +1,4 @@
+import random
 from string import ascii_lowercase
 
 QUESTIONS = [
@@ -7,6 +8,7 @@ QUESTIONS = [
     "I get stressed out easily",
     "I have a rich vocabulary",
     "I don't talk a lot",
+    "I am interested in people",
     "I leave my belongings around",
     "I am relaxed most of the time",
     "I have difficulty understanding abstract ideas",
@@ -34,7 +36,7 @@ QUESTIONS = [
     "I am not really interested in others",
     "I like order",
     "I change my mood a lot",
-    "I m quick to understand things",
+    "I am quick to understand things",
     "I don't like to draw attention to myself",
     "I take time out for others",
     "I shirk my duties",
@@ -52,9 +54,7 @@ QUESTIONS = [
     "I am full of ideas"
 ]
 
-CHOICES = "INACCURATE\t1\t2\t3\t4\t5\tACCURATE"
-
-ALL_USER_CHOICES = []
+ALL_CHOICES = "INACCURATE\t1\t2\t3\t4\t5\tACCURATE"
 
 def start_screen():
     print("To start the OCEAN quiz, enter 's'")
@@ -71,43 +71,56 @@ def start_screen():
             print("Invalid input. Please try again.")
 
 def run_quiz():
+    user_choices = []
+
     for num, question in enumerate(QUESTIONS, start=1):
         print(f"\nQuestion {num}:")
         print(f"{question}?")
-        print(CHOICES)
+        print(ALL_CHOICES)
         while (True):
-            user_choice = input("Choice? ")
+            choice = input("Choice? ")
 
-            if user_choice.isnumeric():
-                ALL_USER_CHOICES.append(user_choice)
+            if choice.isnumeric() and choice.replace(".", "").isnumeric() == False and int(choice) > 0 and int(choice) < 6:
+                user_choices.append(choice)
                 break
             else:
                 print("Invalid number. Please try again.")
     
-    end_screen()
+    # For debugging purposes, ALL_USER_CHOICES will be set to a random
+    # list of 50 choices, ranging from 1 to 5.
+    user_choices = [random.randint(1, 5) for x in range(0, 50)]
 
-def calc_score(ALL_USER_CHOICES):
-    score_E = 20
-    score_A = 14
-    score_C = 14
-    score_N = 38
-    score_O = 8
+    user_scores = calc_score(user_choices)
+    end_screen(user_choices, user_scores)
 
-    score_E_l = []
-    score_A_l = []
-    score_C_l = []
-    score_N_l = []
-    score_O_l = []
+def calc_score(user_choices):
+    # List to store user OCEAN scores. 
+    # score E : scores[0]
+    # score A : scores[1]
+    # score C : scores[2]
+    # score N : scores[3]
+    # score O : scores[4]
+    scores = [20, 14, 14, 38, 8]
 
-    for i in range(0, len(ALL_USER_CHOICES)):
-        question_num = i + 1
+    for num in range(0, len(user_choices), 5):
+        scores[0] += user_choices[num] * (-1) ** num
+        scores[1] -= user_choices[num+1] * (-1) ** num
+        scores[2] += user_choices[num+2] * (-1) ** num
+        scores[3] -= user_choices[num+3] * (-1) ** num
+        scores[4] += user_choices[num+4] * (-1) ** num
 
-        if question_num == (1 OR 6 OR 11 OR 16 OR 21 OR 31 OR 36 OR 41 OR 46):
-            score_E_l.append(question_num) # find better solution
+    return scores
 
-def end_screen():
+def end_screen(user_choices, user_scores):
     print("\nThanks for playing!")
-    
+    print("\nThe following are your scores: ")
+    print("E: " + str(user_scores[0]))
+    print("A: " + str(user_scores[1]))
+    print("C: " + str(user_scores[2]))
+    print("N: " + str(user_scores[3]))
+    print("O: " + str(user_scores[4]) + "\n")
+
+
     while(True):
         user_choice = input("Review your choices? (y/n) ")
         if(user_choice == 'y'):
@@ -115,7 +128,7 @@ def end_screen():
             for num, question in enumerate(QUESTIONS, start=1):
                 print(f"\nQuestion {num}:")
                 print(f"{question}?")
-                print(ALL_USER_CHOICES[num-1])
+                print(user_choices[num-1])
             
             exit(0)
         elif(user_choice == 'n'):
